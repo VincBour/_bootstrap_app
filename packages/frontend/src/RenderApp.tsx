8d21fcbe483f8a54c290f8c64937d20accb5bc36
Vincent Boursicot <vboursicot@talentsoft.com>
2021-04-24 17:11:33 +0200

    add storybook

--- packages/frontend/src/RenderApp.tsx
+++ packages/frontend/src/RenderApp.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { TrainingApp } from './TrainingApp';
 
-ReactDOM.render(<TrainingApp />, document.getElementById('root_training_app'));
+render(<TrainingApp />, document.getElementById('root_training_app'));
