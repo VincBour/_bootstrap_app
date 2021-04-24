2ebec799bdfb670a807d22e2ea17d0d218c3423e
Vincent Boursicot <vboursicot@talentsoft.com>
2021-04-24 13:28:48 +0200

    add react

---
+++ packages/frontend/src/TrainingApp.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export const TrainingApp = () => {
+    return (
+        <div>Hello From TrainingApp</div>
+    )
+};
+
+ReactDOM.render(<TrainingApp />, document.getElementById('root_training_app'));
+
